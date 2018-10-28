d7cd14c656dd98d30f96dca5bf728ccdec285a87
Rui Lopes <rgl@ruilopes.com>
2018-10-28 19:10:09 +0000

    use python3

--- examples/python/list-auth-backends/run.sh
+++ examples/python/list-auth-backends/run.sh
@@ -4,11 +4,11 @@ set -eux
 pushd /vagrant/examples/python/list-auth-backends
 
 # install dependencies.
-apt-get install -y python-pip
-python -m pip install -r requirements.txt
+apt-get install -y python3-pip
+python3 -m pip install -r requirements.txt
 
 # run.
 export REQUESTS_CA_BUNDLE='/etc/ssl/certs/ca-certificates.crt'
-python main.py
+python3 main.py
 
 popd
